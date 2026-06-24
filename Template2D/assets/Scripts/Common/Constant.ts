2d84a94ad73836417df63b5315306435b470824c
PacificaStarlight <139027721+PacificaStarlight@users.noreply.github.com>
2026-06-24 19:19:23 +0800

    Refactor enums and export constants in Constant.ts
    update

--- Template2D/assets/Scripts/Common/Constant.ts
+++ Template2D/assets/Scripts/Common/Constant.ts
@@ -1,103 +1,39 @@
 /** 游戏状态 */
-enum GAME_RUN_STATE {
-    RUNNING = 0,
-    GAMEOVER = 1,
-}
-
-/** 移动类型 */
-enum MOVE_TYPE {
-    NONE = 0,
-    UP = 1,
-    DOWN = 2,
-    LEFT = 3,
-    RIGHT = 4,
+export enum Game_Run_State {
+    Running = 0,
+    GameOver = 1,
 }
 
 /** 事件名字 */
-enum EVENT_TYPE {
+export enum EventName {
     // UI------------------------------------------------------------------------
-    CHANGE_GUIDE = 'ChangeGuide',   // 切换引导界面
-    GAME_START = 'GameStart',   // 游戏开始
-    GAME_OVER = 'GameOver', // 游戏结束
+    Set_Guide_Active = 'Set_Guide_Active',   // 切换引导界面
+    Game_Over = 'GameOver', // 游戏结束
 
     // InputManager-------------------------------------------------------------
-    PLAY_BGM = 'PlayBGM',           // 播放背景音乐
-}
-
-/** 动画名称 */
-enum ANIM_CLIP {
-    BUTTON_JUMP = 'Anims/ButtonJump', // 按钮跳跃动画
+    IsFirTouch = 'IsFirstTouch',           // 是否是第一次触摸
 }
 
 /** 音效类型 */
-enum AUDIO_TYPE {
+export enum AudioName {
     BGM = 'Audios/bgm', // 背景音乐
 
-    BUTTON = 'Audios/button', // 按钮音效
-    CLICK = 'Audios/click', // 点击音效
+    SFX_Button = 'Audios/button', // 按钮音效
+    SFX_Click = 'Audios/click', // 点击音效
 
-    SUCCESS_SFX = 'Audios/success_SFX', // 成功音效
-    FAIL_SFX = 'Audios/success_SFX', // 失败音效
-    POS_SFX_SUCCESS = 'Audios/pos_SFX_success', // 积极反馈音效 - 成功
-    NEG_SFX_FAIL = 'Audios/pos_SFX_success',   // 消极反馈音效 - 失败
-    GUIDE_TIP = 'Audios/guideTip', // 引导提示音效
-    UNLOCK = 'Audios/unlock', // 解锁音效
-    UPGRADE = 'Audios/upgrade', // 升级音效
-    BOARD = 'Audios/board', // 板子音效
-    GETCOIN = 'Audios/getCoin', // 获取金币音效
-    HUIWU = 'Audios/huiwu', // 回物音效
-    SPAWNCOIN = 'Audios/spawnCoin', // 掉落金币音效
-    WAVESOUND = 'Audios/waveSound', // 音浪音效
-}
-
-/** 块类型 */
-export enum BLOCK_TYPE {
-    BLOCK1 = 1,
-    BLOCK2 = 2,
-    BLOCK3 = 3,
-    BLOCK4 = 4,
-    BLOCK5 = 5,
-    BLOCK6 = 6,
-    BLOCK7 = 7,
-    BLOCK8 = 8,
-    BLOCK9 = 9,
-    BLOCK10 = 10,
-    BLOCK11 = 11,
-    BLOCK12 = 12,
-    BLOCK13 = 13,
-    BLOCK14 = 14,
-    BLOCK15 = 15,
+    SFX_Success = 'Audios/success', // 成功音效
+    SFX_Fail = 'Audios/fail', // 失败音效
+    SFX_Right = 'Audios/right', // 积极反馈音效 - 正确
+    SFX_Wrong = 'Audios/wrong',   // 消极反馈音效 - 错误
+    SFX_Upgrade = 'Audios/upgrade', // 升级音效
 }
 
 /** 资源路径 */
-export enum RES_PATH {
-    BLOCK = 'Prefabs/Blocks/Block',
-    PARTICLE = 'Prefabs/Particle',
-    GRID = 'Prefabs/Grids/Grid',
+export enum ResPath {
+    Block = 'Prefabs/Blocks/Block',
 }
 
 /** 常量存储 */
 export class Constant {
-
-    /** 游戏状态 */
-    public static GAME_RUN_STATE = GAME_RUN_STATE;
-
-    /** 移动类型 */
-    public static MOVE_TYPE = MOVE_TYPE;
-
-    /** 事件名字 */
-    public static EVENT_TYPE = EVENT_TYPE;
-
-    /** 资源路径 */
-    public static RES_PATH = RES_PATH;
-
-    /** 动画名称 */
-    public static ANIM_CLIP = ANIM_CLIP;
-
-    /** 音效类型 */
-    public static AUDIO_TYPE = AUDIO_TYPE;
-
-    /** 块类型 */
-    public static BLOCK_TYPE = BLOCK_TYPE;
 
 }
